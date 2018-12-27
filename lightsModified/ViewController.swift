57936d836331a3676310afcc43ed7b092792063a
Chinonso <chinonsoobidike@chinonsos-mbp.bmcc.cuny.edu>
2018-12-27 10:42:30 -0500

    Commit.

--- lightsModified/ViewController.swift
+++ lightsModified/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+var brightness: CGFloat = 1.0;   //1.0 is maximum, 0.0 is minimum
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print("sliderValueChanged \(sender.value)");
+        brightness = CGFloat(sender.value); //Convert Float to CGFloat.
+        updateUI();
+    }
+    
+    func updateUI(){
+        view.backgroundColor = UIColor(white: brightness, alpha: 1.0);
+    }
+    
 }
 
